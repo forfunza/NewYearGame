11f8ea57d59cef02cf240124531ea6483fe7b31c
Bike <Bike@Bikes-MacBook-Pro.local>
2013-11-30 17:01:42 +0700

    Create All Room

---
+++ NewYear/Assets/Scripts/CreateCharacter.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateCharacter : Photon.MonoBehaviour {
+	
+	public bool isEnable = true;
+	public string playerName ;
+	
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnGUI(){
+		if(isEnable){
+			GUI.Window( 0 , new Rect(Screen.width / 2 - 200 , Screen.height /2 - 55 , 400 , 110), InsertName , "Login Page");
+		}
+	}
+	
+	void InsertName(int id){
+		playerName = GUI.TextField( new Rect(20,35,360,30),playerName,16);
+
+
+		if(GUI.Button(new Rect(125,70,150,30),"Login"))
+		{
+		
+			if(playerName == "" || playerName == null)
+			{
+				playerName = "Guest " + Random.Range(0, 10000);
+			}
+
+			PhotonNetwork.playerName = playerName;
+			PhotonNetwork.Connect(PhotonNetwork.PhotonServerSettings.ServerAddress,PhotonNetwork.PhotonServerSettings.ServerPort
+			                      ,PhotonNetwork.PhotonServerSettings.AppID,"0.1");
+		}
+	}
+	
+	void OnConnectedToPhoton(){
+		print(PhotonNetwork.playerName + " Connected ");
+		SelectWorld selectWorld = gameObject.GetComponent<SelectWorld>();
+		selectWorld.isEnable = true;
+		isEnable = false;
+	}
+	
+	void OnFailedToConnectedToPhoton(ExitGames.Client.Photon.StatusCode status){
+		print(status + "FailedConnected");
+		
+	}
+}
---
+++ NewYear/Assets/Scripts/RoomPage.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomPage : Photon.MonoBehaviour {
+	
+	public bool isEnable = false;
+	private string textFieldText = "";
+	private string character;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnGUI(){
+		if(isEnable){
+			GUI.Window( 3 , new Rect(Screen.width / 2 - 300 , Screen.height /2 - 250 , 600 ,500 ), 
+			           RoomUI , PhotonNetwork.room.name);
+		}
+	}
+	
+	void RoomUI(int id){
+
+		if(GUI.Button(new Rect(10 , 450 ,50,30),"Back")){
+			Lobby lobby = gameObject.GetComponent<Lobby>();
+			lobby.isEnable = true;
+			isEnable = false;
+			PhotonNetwork.LeaveRoom();
+		}
+
+		GUI.Box(new Rect(15,60,570,180),"");
+		float currentHeight = 75;
+
+		for(int index =0 ; index < PhotonNetwork.room.playerCount;index++){
+			GUI.Button(new Rect(20,currentHeight,400,30),PhotonNetwork.playerList[index].name);	
+			if(PhotonNetwork.playerList[index].customProperties["Character"].Equals("Boy")){
+				character = "Boy";
+			}else if(PhotonNetwork.playerList[index].customProperties["Character"].Equals("Girl")){
+				character = "Girl";
+			}
+			GUI.Button(new Rect(425,currentHeight,155,30),character);
+			currentHeight = currentHeight + 40;
+		}
+		
+		if(PhotonNetwork.isMasterClient){
+			if(GUI.Button(new Rect(200,455,200,30),"Start Game")){
+				photonView.RPC("StartGame",PhotonTargets.All);
+			}
+		}
+		
+
+	}
+
+}
