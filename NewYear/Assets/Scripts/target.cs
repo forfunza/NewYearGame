64462df13bed5a6c0e48a4fbf866ccd7402cbfcd
Wutthichai <limplemoness@gmail.com>
2013-11-30 16:36:44 +0700

    add item

---
+++ NewYear/Assets/Scripts/target.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class target : MonoBehaviour {
+
+	public static float smooth = 3.0F;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+
+	void Update() {
+		transform.Translate(Vector3.left * smooth * Time.deltaTime);
+	}
+}
